90fca3bfea8432fab9d7242af959af19a4b1c86e
Mustafa J <mustafa.0x@gmail.com>
2026-08-01 22:36:54 +0300

    logger: synchronize default replacement

--- logger/logger.go
+++ logger/logger.go
@@ -4,9 +4,18 @@ import (
 	"log/slog"
 	"os"
 	"sync"
+	"sync/atomic"
 )
 
-var _default Logger = slog.Default()
+type loggerValue struct {
+	Logger
+}
+
+var _default atomic.Value
+
+func init() {
+	_default.Store(loggerValue{Logger: slog.Default()})
+}
 
 type Logger interface {
 	Debug(msg string, args ...any)
@@ -19,24 +28,24 @@ var once sync.Once
 
 func InitLogger(l Logger) {
 	once.Do(func() {
-		_default = l
+		_default.Store(loggerValue{Logger: l})
 	})
 }
 
 func Debug(msg string, args ...any) {
-	_default.Debug(msg, args...)
+	_default.Load().(loggerValue).Debug(msg, args...)
 }
 
 func Info(msg string, args ...any) {
-	_default.Info(msg, args...)
+	_default.Load().(loggerValue).Info(msg, args...)
 }
 
 func Warn(msg string, args ...any) {
-	_default.Warn(msg, args...)
+	_default.Load().(loggerValue).Warn(msg, args...)
 }
 
 func Error(msg string, args ...any) {
-	_default.Error(msg, args...)
+	_default.Load().(loggerValue).Error(msg, args...)
 }
 
 func NewSlog(logLevel slog.Level) Logger {
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,7 +1,38 @@
 package logger
 
-import "testing"
+import (
+	"io"
+	"log/slog"
+	"runtime"
+	"sync"
+	"testing"
+)
 
 func TestDefaultLoggerIsUsable(_ *testing.T) {
 	Debug("default logger is usable")
+}
+
+type testLogger struct {
+	*slog.Logger
+}
+
+func TestInitLoggerWhileLogging(_ *testing.T) {
+	started := make(chan struct{}, 8)
+	var readers sync.WaitGroup
+	for range 8 {
+		readers.Add(1)
+		go func() {
+			defer readers.Done()
+			started <- struct{}{}
+			for range 100_000 {
+				Debug("concurrent logger read")
+			}
+		}()
+	}
+	for range 8 {
+		<-started
+	}
+	runtime.Gosched()
+	InitLogger(testLogger{slog.New(slog.NewTextHandler(io.Discard, nil))})
+	readers.Wait()
 }
